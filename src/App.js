06e0b57343f2690a97ece9adf46f65902b5955d4
Satyam Kumar <satyam73233@gmail.com>
2022-01-22 04:14:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header.js";
+import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBujvGvg1bisJFfFjYyj3NCib_jhNxGh_E",
+  authDomain: "tinder-clone-5cf0d.firebaseapp.com",
+  projectId: "tinder-clone-5cf0d",
+  storageBucket: "tinder-clone-5cf0d.appspot.com",
+  messagingSenderId: "588223984353",
+  appId: "1:588223984353:web:8e59883e72807c6e7dfda0"
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+       <Header />
+       <TinderCards />
+       <SwipeButtons />
     </div>
   );
 }
